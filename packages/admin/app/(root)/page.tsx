e6d5b0f52ee2abf982e683363391af362597d16b
eldarcodes <prog.eldar@gmail.com>
2024-03-27 17:57:24 +0200

    feat: init admin package

---
+++ packages/admin/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p>Hi from admin.</p>
+      <Button>Submit</Button>
+    </>
+  );
+}
