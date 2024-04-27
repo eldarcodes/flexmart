8070328d051717399266d1b70986f26038aebdb4
eldarcodes <prog.eldar@gmail.com>
2024-04-27 15:39:19 +0300

    feat(products): finish product wizard page

--- packages/admin/components/forms/product-form.tsx
+++ packages/admin/components/forms/product-form.tsx
@@ -5,7 +5,7 @@ import { useMutation } from "@tanstack/react-query";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Image, Product } from "@prisma/client";
+import { Category, Color, Image, Product, Size } from "@prisma/client";
 import { toast } from "sonner";
 import { useParams, useRouter } from "next/navigation";
 
@@ -14,24 +14,55 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { ImageUpload } from "@/components/image-upload";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Checkbox } from "@/components/ui/checkbox";
 
 interface ProductFormProps {
   product: (Product & { images: Image[] }) | null;
+  categories: Category[];
+  sizes: Size[];
+  colors: Color[];
 }
 
 const FormSchema = z.object({
   name: z.string().min(2, {
     message: "Product name must be at least 2 characters.",
   }),
+  images: z.object({ url: z.string() }).array(),
+  price: z.coerce.number().positive(),
+  categoryId: z.string().min(2, {
+    message: "Category is required.",
+  }),
+  colorId: z.string().min(2, {
+    message: "Color is required.",
+  }),
+  sizeId: z.string().min(2, {
+    message: "Size is required.",
+  }),
+  isFeatured: z.boolean().default(false).optional(),
+  isArchived: z.boolean().default(false).optional(),
 });
 
-export function ProductForm({ product }: ProductFormProps) {
+export function ProductForm({
+  product,
+  categories,
+  colors,
+  sizes,
+}: ProductFormProps) {
   const router = useRouter();
   const params = useParams();
 
@@ -63,9 +94,21 @@ export function ProductForm({ product }: ProductFormProps) {
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
-    defaultValues: product || {
-      name: "",
-    },
+    defaultValues: product
+      ? {
+          ...product,
+          price: parseFloat(String(product.price)),
+        }
+      : {
+          name: "",
+          images: [],
+          price: 0,
+          categoryId: "",
+          colorId: "",
+          sizeId: "",
+          isFeatured: false,
+          isArchived: false,
+        },
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
@@ -75,6 +118,34 @@ export function ProductForm({ product }: ProductFormProps) {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+        <FormField
+          control={form.control}
+          name="images"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Images</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value.map((image) => image.url)}
+                    disabled={saveProductMutation.isPending}
+                    onChange={(url) =>
+                      field.onChange([...form.watch("images"), { url }])
+                    }
+                    onRemove={(url) =>
+                      field.onChange(
+                        field.value.filter((image) => image.url !== url)
+                      )
+                    }
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+
         <div className="grid grid-cols-3 gap-8">
           <FormField
             control={form.control}
@@ -91,6 +162,171 @@ export function ProductForm({ product }: ProductFormProps) {
                 </FormControl>
 
                 <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Price</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={saveProductMutation.isPending}
+                    placeholder="$9.90"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Category</FormLabel>
+                <Select
+                  disabled={saveProductMutation.isPending}
+                  onValueChange={field.onChange}
+                  value={field.value}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue
+                        defaultValue={field.value}
+                        placeholder="Select a category"
+                      />
+                    </SelectTrigger>
+                  </FormControl>
+
+                  <SelectContent>
+                    {categories.map((category) => (
+                      <SelectItem key={category.id} value={category.id}>
+                        {category.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="sizeId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Size</FormLabel>
+                <Select
+                  disabled={saveProductMutation.isPending}
+                  onValueChange={field.onChange}
+                  value={field.value}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue
+                        defaultValue={field.value}
+                        placeholder="Select a size"
+                      />
+                    </SelectTrigger>
+                  </FormControl>
+
+                  <SelectContent>
+                    {sizes.map((size) => (
+                      <SelectItem key={size.id} value={size.id}>
+                        {size.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="colorId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Color</FormLabel>
+                <Select
+                  disabled={saveProductMutation.isPending}
+                  onValueChange={field.onChange}
+                  value={field.value}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue
+                        defaultValue={field.value}
+                        placeholder="Select a color"
+                      />
+                    </SelectTrigger>
+                  </FormControl>
+
+                  <SelectContent>
+                    {colors.map((color) => (
+                      <SelectItem key={color.id} value={color.id}>
+                        {color.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="isFeatured"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+
+                <div className="space-y-1 leading-none">
+                  <FormLabel>Featured</FormLabel>
+                  <FormDescription>
+                    This product will be displayed on the homepage.
+                  </FormDescription>
+                </div>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="isArchived"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+
+                <div className="space-y-1 leading-none">
+                  <FormLabel>Archived</FormLabel>
+                  <FormDescription>
+                    This product will not appear anywhere in the store.
+                  </FormDescription>
+                </div>
               </FormItem>
             )}
           />
